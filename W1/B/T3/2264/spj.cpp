f5b06bd9b87aa59530fab0e1b81cef475cd0d20b
DarkSharpness_XzY <2040703891@qq.com>
2024-07-02 18:59:05 +0800

    chore(W1-B-T3): modify the grading again

--- W1/B/T3/2264/spj.cpp
+++ W1/B/T3/2264/spj.cpp
@@ -7,14 +7,14 @@ double test(std::istream &in, std::ostream &out) {
     switch (int x; (in >> x), x) {
         case hidden::Corrupted_handle:
             out << "Callback handle corrupted!\n";
-            return 0;
+            return 0.5;
 
         case hidden::Request_too_much:
             out << "Failure after " << hidden::trials << " trials!\n";
             if (unsigned y; in >> y && y <= 43) {
                 out << "Mismatch character count: " << y << '\n';
                 auto ratio = (43 - y) / 43.0;
-                return std::pow(ratio, 10);
+                return std::pow(ratio, 2);
             } else {
                 return 0;
             }
